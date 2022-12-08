b42951526aeb4b8e8990ccd29730fd224b83a805
Obojo <kingvictor009@gmail.com>
2022-12-08 07:26:20 -0800

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,7 +16,7 @@ int main(void)
 	printf("Size of a char: %zu byte(s)\n", sizeof(char_type));
 	printf("Size of an int: %zu byte(s)\n", sizeof(int_type));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(lint_type));
-	printf("Size of a long long int: %zu byte(s)\n", sizeof(llint_type);
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(llint_type));
 	printf("Size of a float: %zu byte(s)\n", sizeof(float_type));
 	return (0);
 }
