5c6fdc29429f8428d67dd792f4851a7f39a29937
Obojo <kingvictor009@gmail.com>
2022-12-08 06:13:48 -0800

    hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,7 @@
 #include <stdio.h>
-/**
- * main - Entry point
- *
- * Return: always 0
- */
-int main(void) 
+
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
