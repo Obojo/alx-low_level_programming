3db1fde801fdd3c52c4786d5039a5486bc97825b
Obojo <kingvictor009@gmail.com>
2022-12-13 19:54:14 -0800

    a+b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * add - add two numbers
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: sum of integers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
