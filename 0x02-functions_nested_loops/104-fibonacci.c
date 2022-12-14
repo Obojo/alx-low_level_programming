1925ed865fe63dff897c6a1422f12f867d697e6b
Obojo <kingvictor009@gmail.com>
2022-12-13 20:53:42 -0800

    98 fib

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+	float a = 1;
+	float b = a + 1;
+	float c = a + b;
+
+	printf("%.0f, ", a);
+	printf("%.0f, ", b);
+	while (i < 96)
+	{
+		i++;
+		printf("%.0f", c);
+		a = b;
+		b = c;
+		c = a + b;
+		if (i < 96)
+		{
+			printf(", ");
+		}
+		else
+		{
+			NULL;
+		}
+	}
+	printf("\n");
+	return (0);
+}
