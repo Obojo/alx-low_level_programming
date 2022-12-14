d4e88196b49ca6e0bd0f92a6fab81c25be5ae9ab
Obojo <kingvictor009@gmail.com>
2022-12-13 19:50:37 -0800

    9x9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,16 +7,16 @@
  */
 void times_table(void)
 {
-    int a;
+	int a;
 	int b;
 	int c;
 
 	for (a = 0; a <= 9; a++)
 	{
-		for (b = 0; b <=9; b++)
+		for (b = 0; b <= 9; b++)
 		{
 			c = a * b;
-			
+
 			if (b == 0)
 			{
 				_putchar('0');
