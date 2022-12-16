ca2c89f94a27094bfc8e148821aaa006d210af23
Obojo <kingvictor009@gmail.com>
2022-12-15 17:53:37 -0800

    largest prime factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	unsigned int a = 2;
+	unsigned long i = 612852475143;
+
+	while (a != n)
+	{
+		if (i % a == 0)
+		{
+			i = i / a;
+		}
+		else
+		{
+			a++;
+		}
+		printf("%lu\n", i);
+		return (0);
+}
