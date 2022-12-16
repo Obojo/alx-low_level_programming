ec6459413502169aa54070775eae4ebabedac535
Obojo <kingvictor009@gmail.com>
2022-12-15 17:01:11 -0800

    numbers and signs

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - display 0-9 with no 2&4
+ *
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int a;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		if (a != '2' || a != '4')
+		{
+			_putchar(a);
+		}
+		else
+		{
+			NULL;
+		}
+	}
+	_putchar('\n');
+}
