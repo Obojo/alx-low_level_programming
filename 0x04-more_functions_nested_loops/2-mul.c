76d0a470500915d5eafa2ede4aaa1f45178958bb
Obojo <kingvictor009@gmail.com>
2022-12-15 16:41:10 -0800

    collaboration

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiply 2 numbers
+ *
+ * @a: first number
+ * @b: second number
+ * Return: multiplication
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
