245615651afb90fb079beb221982b549c78273f4
Obojo <kingvictor009@gmail.com>
2022-12-15 16:35:04 -0800

    isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isdigit - check capitalized alphabets.
+ *
+ * @c: digit
+ * Return: 1 if digit, and 0 other wise.
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '0')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
