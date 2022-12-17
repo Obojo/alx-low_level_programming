4a1f90ccd766bca4a6b06c958f575243aaa95271
Obojo <kingvictor009@gmail.com>
2022-12-16 19:29:11 -0800

    triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_triangle - display a triangle
+ *
+ * @size: size of triangle
+ * Return: triangle
+ */
+void print_triangle(int size)
+{
+	int a;
+	int b;
+	int c;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (a = 1; a <= size; a++)
+		{
+			for (b = 1; b <= (size - a); b++)
+			{
+				_putchar(' ');
+			}
+			for (c = 1; c <= a; c++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
