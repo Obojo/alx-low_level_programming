823c3daeecfacfce57216b7e95b3425da6ea8c39
Obojo <kingvictor009@gmail.com>
2022-12-19 11:15:13 -0800

    Reverse string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include "2-strlen.c"
+/**
+ * print_rev - a string in reverse
+ * @s: the sring
+ *
+ * Return: 0
+ */
+void print_rev(char *s)
+{
+	int i;
+	char temp;
+
+	temp = _strlen(s);
+	for (i = temp; i >= 0; --i)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
