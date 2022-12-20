04a6faeba8c4ecaa8dd35c02ddafef9125448f29
Obojo <kingvictor009@gmail.com>
2022-12-19 16:14:09 -0800

    half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ * puts_half - print half of a string
+ * @str: string
+ *
+ * Return: 0
+ */
+void puts_half(char *str)
+{
+	int i;
+	int temp;
+
+	temp = _strlen(str) - 1;
+	if (temp % 2 == 0)
+	{
+		for (i = (temp / 2); i < temp ; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	else
+	{
+		for (i = (temp + 1) / 2; i < (temp + 1); i++)
+		{
+			_putchar(*(str + i));
+		}
+	}
+	_putchar('\n');
+}
